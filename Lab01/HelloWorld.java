4cdf9cd8fbd13b9a17837912c7308a973c7e3b79
Nguyễn Khắc Hiếu <163430809+mcgee12310@users.noreply.github.com>
2024-09-28 09:02:41 +0700

    Add files via upload

---
+++ Lab01/HelloWorld.java
@@ -0,0 +1,8 @@
+//Example 1: HelloWorld.java 2
+//Text-printing program 
+public class HelloWorld{
+    public static void main(String args[]){ 
+        System.out.println("Xin chao \n cac ban!"); 
+        System.out.println("Hello \t world!");
+    } //end of method main
+} 
